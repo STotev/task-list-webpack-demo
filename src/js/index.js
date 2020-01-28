b2de9e521e511a027fac92edaa2c4273a0beef08
Stefan Totev <stefan.totev@espc.com>
2020-01-28 09:27:18 +0000

    Clearing up predefined data

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,6 @@
 const app = new App();
 const taskList = new TaskList();
 
-taskList.add(new Task('Some title', 'Some description', TaskStage.TODO));
-taskList.add(new Task('Some title 2', 'Some description 2', TaskStage.IN_PROGRESS));
-taskList.add(new Task('Some title 3', 'Some description 3', TaskStage.IN_PROGRESS));
-taskList.add(new Task('Some title 4', 'Some description 4', TaskStage.TODO));
-taskList.add(new Task('Some title 5', 'Some description 5', TaskStage.DONE));
-taskList.add(new Task('Some title 6', 'Some description 6', TaskStage.ON_HOLD));
-taskList.add(new Task('Some title 7', 'Some description 7', TaskStage.DONE));
-taskList.add(new Task('Some title 8', 'Some description 8', TaskStage.IN_PROGRESS));
-taskList.add(new Task('Some title 9', 'Some description 9', TaskStage.IN_PROGRESS));
-
 app.registerTaskList(taskList);
 
 app.registerStage(new Stage("#todo",        TaskStage.TODO));
